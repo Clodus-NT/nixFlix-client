ceec39bc863d59685374c89d45f69dd7782a74fb
Clodus-NT <clodus.nt@gmail.com>
2022-04-26 15:29:27 -0400

    added stylings to movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Button, Card, CardGroup, Container, Row, Col} from 'react-bootstrap';
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
     keypressCallback(event) {
@@ -17,20 +19,37 @@ export class MovieView extends React.Component {
         const { movie, onBackClick } = this.props;
 
         return (
-            <div className="movie-view">
-                <div className="movie-poster">
-                    <img crossOrigin="*" src={movie.ImagePath} />
-                </div>
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title}</span>
-                </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description}</span>
-                </div>
-                <button onClick={() => {onBackClick(null); }}>Back</button>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Img crossOrigin="*" src={movie.ImagePath} />
+                                <Card.Title>{movie.Title}</Card.Title>
+                                <Card.Text>Genre: {movie.Genre.Name}</Card.Text>
+                                <Card.Text>Synopsis: {movie.Description} </Card.Text>
+                                <Card.Text>Director: {movie.Director.Name} </Card.Text>
+                                <Card.Text> About the director: {movie.Director.Bio} </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Button onClick={() => {onBackClick(null); }}>Back</Button>
+                    </Col>
+                </Row>
+            </Container>
+            // <div className="movie-view">
+            //     <div className="movie-poster">
+            //         <img crossOrigin="*" src={movie.ImagePath} />
+            //     </div>
+            //     <div className="movie-title">
+            //         <span className="label">Title: </span>
+            //         <span className="value">{movie.Title}</span>
+            //     </div>
+            //     <div className="movie-description">
+            //         <span className="label">Description: </span>
+            //         <span className="value">{movie.Description}</span>
+            //     </div>
+            //     <button onClick={() => {onBackClick(null); }}>Back</button>
+            // </div>
 
         );
     }
