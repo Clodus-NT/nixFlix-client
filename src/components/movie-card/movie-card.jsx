476e8f4679db1e04a5a0e512ee2a883b7dcedf0e
Clodus-NT <clodus.nt@gmail.com>
2022-04-27 13:44:58 -0400

    refactured MovieCard return block to allow navigation to MovieView

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Button, Card, CardGroup, Container, Row, Col} from 'react-bootstrap';
 
+import { Link } from "react-router-dom";
+
 import './movie-card.scss';
 
 
 export class MovieCard extends React.Component {
     render() {
-        const { movie, onMovieClick } = this.props;
+        const { movie/*}, onMovieClick*/} = this.props;
         console.log(movie);
         return (
             <Container>
@@ -19,7 +21,10 @@ export class MovieCard extends React.Component {
                                     <Card.Body>
                                         <Card.Title>{movie.Title}</Card.Title>
                                         <Card.Text>Director: {movie.Director.Name}</Card.Text>
-                                        <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                                        <Link to={`/movies/${movie._id}`}>
+                                            <Button variant="link">See more</Button>
+                                        </Link>
+                                        {/* <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button> */}
                                     </Card.Body>
                             </Card>
                         </CardGroup>
